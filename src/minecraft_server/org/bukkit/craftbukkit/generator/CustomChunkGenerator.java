322c3dff27e391e530e8388a305e3f68cced9849
vladthemountain <vg07102004@gmail.com>
2022-11-13 12:03:05 +0300

    NormalChunkGenerator and CustomChunkGenerator

--- src/minecraft_server/org/bukkit/craftbukkit/generator/CustomChunkGenerator.java
+++ src/minecraft_server/org/bukkit/craftbukkit/generator/CustomChunkGenerator.java
@@ -4,72 +4,71 @@ import net.minecraft.src.Chunk;
 import net.minecraft.src.IChunkProvider;
 import net.minecraft.src.IProgressUpdate;
 import net.minecraft.src.World;
+import net.minecraft.src.WorldServer;
+import org.bukkit.generator.BlockPopulator;
 import org.bukkit.generator.ChunkGenerator;
 
+import java.util.List;
 import java.util.Random;
 
 @Deprecated
 public class CustomChunkGenerator extends InternalChunkGenerator {
 
+    private final ChunkGenerator generator;
+    private final WorldServer world;
+    private final Random random;
+
     public CustomChunkGenerator(World world, long seed, ChunkGenerator generator) {
-        throw new UnsupportedOperationException("One of the plugins called deprecated Chunk Generation API");
+        this.world = (WorldServer) world;
+        this.generator = generator;
+
+        this.random = new Random(seed);
     }
 
-    @Override
-    public boolean chunkExists(int x, int z) {
-        throw new UnsupportedOperationException("One of the plugins called deprecated Chunk Generation API");
+    public boolean isChunkLoaded(int x, int z) {
+        return true;
     }
 
-    @Override
-    public Chunk provideChunk(int x, int z) {
-        throw new UnsupportedOperationException("One of the plugins called deprecated Chunk Generation API");
+    public Chunk getOrCreateChunk(int x, int z) {
+        random.setSeed((long) x * 341873128712L + (long) z * 132897987541L);
+        byte[] types = generator.generate(world.getWorld(), random, x, z);
+
+        Chunk chunk = new Chunk(world, types, x, z);
+
+        chunk.initLighting();
+
+        return chunk;
     }
 
-    @Override
-    public void populate(IChunkProvider chunkProvider, int x, int z) {
-        throw new UnsupportedOperationException("One of the plugins called deprecated Chunk Generation API");
+    public void getChunkAt(IChunkProvider icp, int i, int i1) {
+        // Nothing!
     }
 
-    @Override
-    public boolean saveChunks(boolean flag, IProgressUpdate progressUpdate) {
-        throw new UnsupportedOperationException("One of the plugins called deprecated Chunk Generation API");
+    public boolean saveChunks(boolean bln, IProgressUpdate ipu) {
+        return true;
     }
 
-    @Override
-    public boolean unload100OldestChunks() {
-        throw new UnsupportedOperationException("One of the plugins called deprecated Chunk Generation API");
+    public boolean unloadChunks() {
+        return false;
     }
 
-    @Override
     public boolean canSave() {
-        throw new UnsupportedOperationException("One of the plugins called deprecated Chunk Generation API");
+        return true;
     }
 
-    /**
-     * Shapes the chunk for the given coordinates.<br>
-     * <br>
-     * This method should return a byte[32768] in the following format:
-     * {@code
-     * for (int x = 0; x < 16; x++) {
-     * for (int z = 0; z < 16; z++) {
-     * for (int y = 0; y < 128; y++) {
-     * // result[(x * 16 + z) * 128 + y] = ??;
-     * }
-     * }
-     * }
-     * }
-     *
-     * Note that this method should <b>never</b> attempt to get the Chunk at
-     * the passed coordinates, as doing so may cause an infinite loop
-     *
-     * @param world  The world this chunk will be used for
-     * @param random The random generator to use
-     * @param x      The X-coordinate of the chunk
-     * @param z      The Z-coordinate of the chunk
-     * @return byte[] containing the types for each block created by this generator
-     */
-    @Override
     public byte[] generate(org.bukkit.World world, Random random, int x, int z) {
-        throw new UnsupportedOperationException("One of the plugins called deprecated Chunk Generation API");
+        return generator.generate(world, random, x, z);
+    }
+
+    public Chunk getChunkAt(int x, int z) {
+        return getOrCreateChunk(x, z);
+    }
+
+    public boolean canSpawn(org.bukkit.World world, int x, int z) {
+        return generator.canSpawn(world, x, z);
+    }
+
+    public List<BlockPopulator> getDefaultPopulators(org.bukkit.World world) {
+        return generator.getDefaultPopulators(world);
     }
 }
